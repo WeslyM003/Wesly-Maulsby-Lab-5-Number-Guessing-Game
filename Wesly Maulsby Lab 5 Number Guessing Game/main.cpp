e79871e2f950b8fdbc2a1abe5abf705873ca609b
WeslyM003 <maulsbywes@gmail.com>
2022-11-02 10:27:10 -0500

    final

--- Wesly Maulsby Lab 5 Number Guessing Game/main.cpp
+++ Wesly Maulsby Lab 5 Number Guessing Game/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+
 #include <cstdlib>
+
 #include <ctime>
 using namespace std;
 
@@ -14,6 +16,8 @@ int playAgain;
 
 int main()
 {
+	int wins = 0;
+	int losses = 0;
 
 	do
 	{
@@ -21,14 +25,15 @@ int main()
 		int guess = 0;
 		int num = 0;
 		int tries = 0;
+		srand(time(0)); //seed random number generator
+		num = rand() % 100 + 1; // random number between 1 and 100
+		cout << "Guess My Number Game\n\n";
 
 		do
 		{
 			
-			int num, guess, tries = 0;	//im actually not sure why this is here, the internet told me to do this but??
-			srand(time(0)); //seed random number generator
-			num = rand() % 100 + 1; // random number between 1 and 100
-			cout << "Guess My Number Game\n\n";
+
+
 
 			cout << num << endl;  //prints the number to guess for testing purposes, will be removed later
 
@@ -40,17 +45,24 @@ int main()
 			if (guess != num && tries < 20)
 				cout << getResponseIncorrect() << "\n\n";
 			else if (guess == num && tries < 20)
+			{
 				cout << getResponseCorrect() << "\nYou got it in " << tries << " guesses!\n";
+				wins = wins + 1;
+			}
 			else if (tries == 20)
-				cout << getResponseLose();
+			{
+				cout << getResponseLose() << endl;
+				losses = losses + 1;
+			}
 
-		} while (guess != num && tries <= 20);
+		} while (guess != num && tries <= 19);
 
 		//determines if the user wants to play again or exit the game.
 		cout << "Do you want to play again, enter 1 for yes, enter any other number for no: ";
 		cin >> playAgain;
 	} while (playAgain == 1);
 
+	cout << endl << "WINS: " << wins << endl << "LOSSES: " << losses;
 
 	return 0;
 }
@@ -59,29 +71,33 @@ int main()
 string getResponseIncorrect()
 {
 	string response = "";
-	srand(time(0)); //seed random number generator
 	int responseNum = rand() % 10 + 1; // random number between 1 and 10
 
-	if (responseNum == 1)
-		response = "Nope";
-	else if (responseNum == 2)
-		response = "Nada";
-	else if (responseNum == 3)
-		response = "That aint it Chief";
-	else if (responseNum == 4)
-		response = "Not Quite";
-	else if (responseNum == 5)
-		response = "No";
-	else if (responseNum == 6)
-		response = "Wrong";
-	else if (responseNum == 7)
-		response = "Incorrect";
-	else if (responseNum == 8)
-		response = "Wrongo";
-	else if (responseNum == 9)
-		response = "You're Wrong";
-	else if (responseNum == 10)
-		response = "Nerp";
+	switch (responseNum)
+	{
+	case 1: response = "Nope";
+		break;
+	case 2: response = "Nada";
+		break;
+	case 3: response = "That aint it Chief";
+		break;
+	case 4: response = "Nerp";
+		break;
+	case 5: response = "Not Quite";
+		break;
+	case 6: response = "No";
+		break;
+	case 7: response = "Wrong";
+		break;
+	case 8: response = "Incorrect";
+		break;
+	case 9: response = "Wrongo";
+		break;
+	case 10: response = "You're Wrong";
+		break;
+	default:
+		break;
+	}
 
 	return response;
 
@@ -91,30 +107,33 @@ string getResponseIncorrect()
 string getResponseCorrect()
 {
 	string response = "";
-	srand(time(0)); //seed random number generator
 	int responseNum = rand() % 10 + 1; // random number between 1 and 10
 
-
-	if (responseNum == 1)
-		response = "Correct!";
-	else if (responseNum == 2)
-		response = "You got it!";
-	else if (responseNum == 3)
-		response = "Thats it!";
-	else if (responseNum == 4)
-		response = "Thats right!";
-	else if (responseNum == 5)
-		response = "Yes";
-	else if (responseNum == 6)
-		response = "You guessed correctly!";
-	else if (responseNum == 7)
-		response = "Yes Sir!";
-	else if (responseNum == 8)
-		response = "https://www.youtube.com/watch?v=OgZzUJud3Q4";
-	else if (responseNum == 9)
-		response = "Good job Chief!";
-	else if (responseNum == 10)
-		response = "Way to go Bossman";
+	switch (responseNum)
+	{
+	case 1: response = "Correct!";
+		break;
+	case 2: response = "You got it!";
+		break;
+	case 3: response = "Thats it!";
+		break;
+	case 4: response = "Thats right!";
+		break;
+	case 5: response = "Yes";
+		break;
+	case 6: response = "You guessed correctly!";
+		break;
+	case 7: response = "Yes Sir!";
+		break;
+	case 8: response = "https://www.youtube.com/watch?v=OgZzUJud3Q4";
+		break;
+	case 9: response = "Good job Chief!";
+		break;
+	case 10: response = "Way to go Bossman";
+		break;
+	default:
+		break;
+	}
 
 	return response;
 
@@ -124,30 +143,33 @@ string getResponseCorrect()
 string getResponseLose()
 {
 	string response = "";
-	srand(time(0)); //seed random number generator
 	int responseNum = rand() % 10 + 1; // random number between 1 and 10
 
-	if (responseNum == 1)
-		response = "You Lose!";
-	else if (responseNum == 2)
-		response = "Failed";
-	else if (responseNum == 3)
-		response = "Failure";
-	else if (responseNum == 4)
-		response = "Dumb Dumb";
-	else if (responseNum == 5)
-		response = "You Failed to Guess the Number!";
-	else if (responseNum == 6)
-		response = "*The Price is Right Failure soud*";
-	else if (responseNum == 7)
-		response = "https://www.youtube.com/watch?v=OgZzUJud3Q4";
-	else if (responseNum == 8)
-		response = "Your lack of correct guesses disturbs me";
-	else if (responseNum == 9)
-		response = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-	else if (responseNum == 10)
-		response = "https://www.youtube.com/watch?v=_asNhzXq72w";
+	switch (responseNum)
+	{
+	case 1: response = "You Lose!";
+		break;
+	case 2: response = "Failed";
+		break;
+	case 3: response = "Failure";
+		break;
+	case 4: response = "Dumb Dumb";
+		break;
+	case 5: response = "You Failed to Guess the Number!";
+		break;
+	case 6: response = "*The Price is Right Failure soud*";
+		break;
+	case 7: response = "https://www.youtube.com/watch?v=OgZzUJud3Q4";
+		break;
+	case 8: response = "Your lack of correct guesses disturbs me";
+		break;
+	case 9: response = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+		break;
+	case 10: response = "https://www.youtube.com/watch?v=_asNhzXq72w";
+		break;
+	default:
+		break;
+	}
 
 	return response;
-
 }
